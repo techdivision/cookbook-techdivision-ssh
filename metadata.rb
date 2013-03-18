8dfb2dee228b529b50c4724797985c439668311d
Robert Lemke <rl@robertlemke.com>
2013-03-18 15:26:17 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'robertlemke-ssh'
+maintainer       'Robert Lemke'
+maintainer_email 'rl@robertlemke.com'
+license          'MIT'
+description      'Provides SSH related functions, such as setting authorized_keys'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+supports "ubuntu"
+supports "debian"
---
+++ providers/authorized_keys_entry.rb
@@ -0,0 +1,52 @@
+#
+# Author:: Robert Lemke <rl@robertlemke.com>
+# Provider:: authorized_keys_entry
+#
+# Copyright (c) 2013 Robert Lemke
+#
+# Licensed under the MIT License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+
+action :create do
+
+    # FIXME: make path configurable through "user" attribute:
+  authorizedKeysPath = "/root/.ssh/"
+  authorizedKeysFilename = "authorized_keys"
+  authorizedKeysPathAndFilename = "#{authorizedKeysPath}#{authorizedKeysFilename}"
+
+  recipe_eval do
+
+    directory "#{authorizedKeysPath}" do
+      user new_resource.user
+      group "root"
+      mode 00700
+    end
+
+  end
+
+  # Ensure that the file exists and has minimal content (required by Chef::Util::FileEdit)
+  file authorizedKeysPathAndFilename do
+    action :create
+    backup false
+    content '# Authorized Keys'
+    user new_resource.user
+    group "root"
+    mode 00600
+    only_if do
+      !::File.exists?(authorizedKeysPathAndFilename) || ::File.new(authorizedKeysPathAndFilename).readlines.length == 0
+    end
+  end
+
+  ruby_block "add #{new_resource.key} to #{authorizedKeysPathAndFilename}" do
+    block do
+      file = ::Chef::Util::FileEdit.new(authorizedKeysPathAndFilename)
+      file.insert_line_if_no_match(/#{Regexp.escape(new_resource.key)}/, new_resource.key)
+      file.write_file
+    end
+  end
+  new_resource.updated_by_last_action(true)
+end
