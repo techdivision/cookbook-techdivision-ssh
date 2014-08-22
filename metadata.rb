bcf0ff6feb32c25c0050eaa0b57221759d62c055
Robert Lemke <r.lemke@techdivision.com>
2014-08-22 12:48:14 +0200

    [TASK] Bump version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'r.lemke@techdivision.com'
 license          'MIT'
 description      'Provides SSH related functions, such as setting authorized_keys'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 supports "ubuntu"
 supports "debian"
